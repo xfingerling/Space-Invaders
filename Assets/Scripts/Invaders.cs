f7b1a04da01ebef8bfca23bb05d9a36566bd6d83
Oleg Zavhorodnii <oleggenius95@gmail.com>
2022-06-13 16:46:44 +0300

    invaders movement

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -5,8 +5,41 @@ public class Invaders : MonoBehaviour
     [SerializeField] private Invader[] _prefabs;
     [SerializeField] private int _rows = 5;
     [SerializeField] private int _columns = 11;
+    [SerializeField] private float _speed = 1f;
+
+    private Vector3 _direction = Vector2.right;
 
     private void Awake()
+    {
+        SpawnInvaders();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position += _direction * _speed * Time.deltaTime;
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        foreach (Transform invader in transform)
+        {
+            if (!invader.gameObject.activeInHierarchy)
+                continue;
+
+            if (_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1))
+                AdvanceRow();
+            else if (_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1))
+                AdvanceRow();
+
+        }
+    }
+
+    private void SpawnInvaders()
     {
         for (int row = 0; row < _rows; row++)
         {
@@ -24,5 +57,14 @@ public class Invaders : MonoBehaviour
                 invader.transform.localPosition = position;
             }
         }
+    }
+
+    private void AdvanceRow()
+    {
+        _direction.x *= -1;
+
+        Vector3 position = transform.position;
+        position.y -= 1;
+        transform.position = position;
     }
 }
